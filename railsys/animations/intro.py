28b037cfb3d18711797589c974557136eb5c5aeb
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2021-04-18 01:21:27 +0530

    Update intro.py
    Reduced code length and also corrected `clear` which doesnot work on command prompt.
    its cls command

--- railsys/animations/intro.py
+++ railsys/animations/intro.py
@@ -7,47 +7,8 @@ import time
 import os
 
 def animate():
-    print(render("R", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RA", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAI", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAIL", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILW", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWA", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY", colors=['red', 'yellow'], align='center'))
-    os.system('clear')
-    print(render("RAILWAY M", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY MG", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY MGM", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY MGMT", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY MGMT S", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY MGMT SY", colors=['red', 'yellow'], align='center'))
-    time.sleep(0.2)
-    os.system('clear')
-    print(render("RAILWAY MGMT SYS", colors=['red', 'yellow'], align='center'))
-
-
-
-
-
+    word="RAILWAY MGMT SYS"
+    for i in range(1,len(word)+1):
+        print(render(word[0:i], colors=['red', 'yellow'], align='center'))
+        time.sleep(0.2)
+        os.system('cls')
